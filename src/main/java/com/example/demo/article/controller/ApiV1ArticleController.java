3ad0cc173b5ed9a438ac8c38a2c64939ac728c64
깃íkaeng2258 <kaeng2258@gmail.com>
2025-09-19 16:49:43 +0900

    create,list,select,modify

--- src/main/java/com/example/demo/article/controller/ApiV1ArticleController.java
+++ src/main/java/com/example/demo/article/controller/ApiV1ArticleController.java
@@ -1,10 +1,20 @@
 package com.example.demo.article.controller;
 
 
+import com.example.demo.article.dto.ArticleDTO;
+import com.example.demo.article.entity.Article;
 import com.example.demo.article.service.ArticleService;
+import com.example.demo.global.RsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/articles")
@@ -13,26 +23,70 @@ public class ApiV1ArticleController {
 
     private final ArticleService articleService;
 
+
+    @Getter
+    @AllArgsConstructor
+    public static class ArticlesResponse{
+        private final List<ArticleDTO> articleList;
+    }
+
     @GetMapping("")
-    public String list(){
-        return "list";
+    public RsData<ArticlesResponse> list() {
+        List<ArticleDTO> articleList = new ArrayList<>();
+
+        Article article1 = new Article("제목 1", "내용1");
+        articleList.add(new ArticleDTO(article1));
+
+        Article article2 = new Article("제목 2", "내용2");
+        articleList.add(new ArticleDTO(article2));
+
+        Article article3 = new Article("제목 3", "내용3");
+        articleList.add(new ArticleDTO(article3));
+        return RsData.of("200", "게시글다건조회성공", new ArticlesResponse(articleList));
     }
+
+    @Getter
+    @AllArgsConstructor
+    public static class ArticleResponse{
+        private final ArticleDTO article;
+    }
+
     @GetMapping("/{id}")
-    public String getArticle(){
-        return "one";
+    public RsData<ArticleResponse> getArticle(@PathVariable("id") Long id) {
+        Article article = new Article("제목1", "내용1");
+
+        ArticleDTO articleDTO = new ArticleDTO(article);
+
+        return RsData.of("200", "게시글단건조회성공", new ArticleResponse(articleDTO));
     }
+
+    @Data
+    public static class ArticleRequest{
+        @NotBlank
+        private String subject;
+
+        @NotBlank
+        private String content;
+    }
+
     @PostMapping("")
-    public String crete(){
-        return "";
+    public String crete(@Valid @RequestBody ArticleRequest articleRequest) {
+        System.out.println(articleRequest.subject);
+        System.out.println(articleRequest.content);
+        return "등록";
     }
 
     @PatchMapping("/{id}")
-    public String modify(){
-        return "";
+    public String modify(@PathVariable("id") Long id, @RequestParam("subject") String subject, @RequestParam("content") String content) {
+        System.out.println(id);
+        System.out.println(subject);
+        System.out.println(content);
+        return "수정";
     }
 
-    @DeleteMapping("{id}")
-    public String delete(){
-        return "";
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        System.out.println(id);
+        return "삭제";
     }
 }
